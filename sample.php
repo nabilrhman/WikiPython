adc815c3ce6772c84e7ac5cd4e83f307a5879a30
Nabil Rahman <nabilr@outlook.com>
2018-11-07 22:46:18 -0700

    Adds a new php file named sample.php for sample programs.

---
+++ sample.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html class="no-js">
+<head>
+    <!-- Basic Page Needs
+    ================================================== -->
+    <meta charset="utf-8">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <link rel="icon" href="favicon.ico">
+    <title>WikiPython - Home</title>
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+    <meta name="author" content="">
+    <!-- Mobile Specific Metas
+    ================================================== -->
+    <meta name="format-detection" content="telephone=no">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+
+    <!-- Template CSS Files
+    ================================================== -->
+    <!-- Twitter Bootstrs CSS -->
+    <link rel="stylesheet" href="plugins/bootstrap/bootstrap.min.css">
+    <!-- Ionicons Fonts Css -->
+    <link rel="stylesheet" href="plugins/ionicons/ionicons.min.css">
+    <!-- animate css -->
+    <link rel="stylesheet" href="plugins/animate-css/animate.css">
+    <!-- Hero area slider css-->
+    <link rel="stylesheet" href="plugins/slider/slider.css">
+    <!-- owl craousel css -->
+    <link rel="stylesheet" href="plugins/owl-carousel/owl.carousel.css">
+    <link rel="stylesheet" href="plugins/owl-carousel/owl.theme.css">
+    <!-- Fancybox -->
+    <link rel="stylesheet" href="plugins/facncybox/jquery.fancybox.css">
+    <!-- template main css file -->
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+<?php include 'header.php'; ?>
+
+
+<!--
+==================================================
+    Global Page Section Start
+================================================== -->
+<section class="global-page-header">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="block">
+                    <h2>Home</h2>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<!--
+==================================================
+    Company Description Section Start
+================================================== -->
+<section class="company-description">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 wow fadeInLeft" data-wow-delay=".3s">
+                <img src="images/home/python.jpg" alt="" class="img-responsive">
+            </div>
+            <div class="col-md-6">
+                <div class="block">
+                    <h3 class="subtitle wow fadeInUp" data-wow-delay=".3s" data-wow-duration="500ms">
+                        History of Python
+                    </h3>
+                    <p class="wow fadeInUp" data-wow-delay=".5s" data-wow-duration="500ms">
+                        Python is an interpreted high-level programming language for general-purpose programming.
+                        Created by Guido van Rossum and first released in 1991, Python has a design philosophy that
+                        emphasizes code readability, notably using significant whitespace. It provides constructs that
+                        enable clear programming on both small and large scales. In July 2018, Van Rossum stepped down
+                        as the leader in the language community after 30 years.
+                    </p>
+                    <p class="wow fadeInUp" data-wow-delay=".7s" data-wow-duration="500ms">
+                        Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et
+                        voluptates repudiandae sint et molestiae non recusandae.
+                    </p>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<?php include 'footer.php'; ?>
+
+
+<!-- Template Javascript Files
+================================================== -->
+<!-- jquery -->
+<script src="plugins/jQurey/jquery.min.js"></script>
+<!-- Form Validation -->
+<script src="plugins/form-validation/jquery.form.js"></script>
+<script src="plugins/form-validation/jquery.validate.min.js"></script>
+<!-- owl carouserl js -->
+<script src="plugins/owl-carousel/owl.carousel.min.js"></script>
+<!-- bootstrap js -->
+<script src="plugins/bootstrap/bootstrap.min.js"></script>
+<!-- wow js -->
+<script src="plugins/wow-js/wow.min.js"></script>
+<!-- slider js -->
+<script src="plugins/slider/slider.js"></script>
+<!-- Fancybox -->
+<script src="plugins/facncybox/jquery.fancybox.js"></script>
+<!-- template main js -->
+<script src="js/main.js"></script>
+</body>
+</html>
