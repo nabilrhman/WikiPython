ba5f3a2d88e342bf2007ab17f245fb3cfd6135af
Nabil Rahman <nabilr@outlook.com>
2018-11-07 22:27:19 -0700

    Initializes the project.

---
+++ footer.php
@@ -0,0 +1,34 @@
+<!--
+            ==================================================
+            Footer Section Start
+            ================================================== -->
+            <footer id="footer">
+                <div class="container">
+                    <div class="col-md-8">
+                        <p class="copyright">Copyright: <span><script>document.write(new Date().getFullYear())</script></span> Team Hawks. <br>
+                            Developed by
+                            <a href="https://themefisher.com/free-bootstrap-templates/" target="_blank">
+                                Emanuel Hernandez
+                            </a>
+                            |
+                            <a href="https://themefisher.com/free-bootstrap-templates/" target="_blank">
+                                Garrett Smith
+                            </a>
+                            |
+                            <a href="https://themefisher.com/free-bootstrap-templates/" target="_blank">
+                                Nabil Rahman
+                            </a>
+                        </p>
+                    </div>
+                    <div class="col-md-4">
+                        <!-- Social Media -->
+                        <ul class="social">
+                            <li>
+                                <a href="http://wwww.fb.com/themefisher" class="GitHub">
+                                    <i class="ion-social-github"></i>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </footer> <!-- /#footer -->
---
+++ header.php
@@ -0,0 +1,40 @@
+<!--
+        ==================================================
+        Header Section Start
+        ================================================== -->
+        <header id="top-bar" class="navbar-fixed-top animated-header">
+            <div class="container">
+                <div class="navbar-header">
+                    <!-- responsive nav button -->
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    </button>
+                    <!-- /responsive nav button -->
+                    
+                    <!-- logo -->
+                    <div class="navbar-brand">
+                        <a href="index.html" >
+                            <img src="images/logo.png" alt="">
+                        </a>
+                    </div>
+                    <!-- /logo -->
+                </div>
+                <!-- main menu -->
+                <nav class="collapse navbar-collapse navbar-right" style="margin-top: 20px !important;" role="navigation">
+                    <div class="main-menu">
+                        <ul class="nav navbar-nav navbar-right">
+                            <li><a href="index.html" >Home</a></li>
+                            <li><a href="interpreter.html">Interpreter</a></li>
+                            <li><a href="sample.html">Sample programs</a></li>
+                            <li><a href="comparison.html">Comparison</a></li>
+                            <li><a href="utility.html">Utility</a></li>
+                        </ul>
+                    </div>
+                </nav>
+                <!-- /main nav -->
+            </div>
+        </header>
+        
